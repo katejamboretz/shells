cd9b4da663e852311f6006d1661f5e0630ae6976
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-06-06 19:23:51 -0700

    remove some comments, test deployment

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import image_1 from "../../images/aaron-burden-1rVQGaA_toU-unsplash.jpg"
+
 
 function Card(props) {
     return (
@@ -10,5 +10,5 @@ function Card(props) {
         </div>
     )
 }
-// style=" width: 18rem;">    Can't figure out how to add this style    {props.image}
+
 export default Card;
